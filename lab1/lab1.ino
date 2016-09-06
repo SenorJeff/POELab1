7c82e0d33c27474b7671aa5738863992796f729f
Jeff Pflueger <jeffrey.pflueger@students.olin.edu>
2016-09-06 10:06:54 -0400

    changed code to work with IR distance sensor and added button states

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -27,15 +27,17 @@
 // constants won't change. They're used here to
 // set pin numbers:
 const int buttonPin = 8;     // the number of the pushbutton pin
-const int green =  12;// the number of the LED pin
-const int yellow = 11;
-const int red = 10;
+const int green =  11;// the number of the LED pin
+const int yellow = 10;
+const int red = 9;
+const int sensorPin = A0;
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 int LEDState = 0;
 int previousButtonState = 0;
-
+int timer; 
+int sensorValue;
 void setup() {
   // initialize the LED pin as an output:
   pinMode(green, OUTPUT);
@@ -43,21 +45,21 @@ void setup() {
   pinMode(red, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
-
   // initialize Serial Communication
   Serial.begin(9600);
 }
 
 void writeAll(bool redon, bool yellowon, bool greenon){
-  digitalWrite(red, redon);
-  digitalWrite(yellow, yellowon);
-  digitalWrite(green, greenon);
+  analogWrite(red, 250-sensorValue/2);
+  analogWrite(yellow, 250-sensorValue/2);
+  analogWrite(green,250-sensorValue/2);
 }
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if ((buttonState == HIGH) && (previousButtonState == LOW)) {
@@ -67,8 +69,27 @@ void loop() {
       Serial.println(LEDState);
   }
 
-  bool greenOn = LEDState > 2;
-  writeAll(greenOn, greenOn, HIGH);
+  timer = millis()/500;
+  if (LEDState == 0)
+  {
+    writeAll(HIGH, HIGH, HIGH);
+  }
+  else if(LEDState == 1)
+  {
+    writeAll(timer%2 > 0, timer%2 > 0, timer%2 > 0);
+  }
+  else if(LEDState == 2)
+  {
+    writeAll(timer%2 > 0,timer%4 > 1, timer%8 > 3);
+  }
+  else if(LEDState == 3)
+  {
+    writeAll(timer%3 == 0, timer%3 == 1, timer%3 == 2);
+  }
+  else if(LEDState == 4)
+  {
+    writeAll(LOW, LOW, LOW);
+  }
   
   previousButtonState = buttonState;
   delay(100);
