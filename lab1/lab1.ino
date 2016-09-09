109e7b83f89b98063f32c594e12f3519fef3751c
Jeff Pflueger <jeffrey.pflueger@students.olin.edu>
2016-09-09 10:14:12 -0400

    Added comments and made changes for readability

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,55 +1,64 @@
 /*
-  Button
+  Lab1
+  9/9/2016
+  Jeff Pflueger and Eric Miller
 
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
+  This code satisfies the requirements for Lab 1
 
+  LEDs are hooked up to pins 9, 10, and 11
+  A button is hooked up to pin 8
+  An Infra-Red distance sensor is hooked up to pin A0
 
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
+   The Button switches between 9 states for the LEDs:
 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
+   All On
+   Blinking
+   Counting forward in Binary
+   Counting backward in Binary
+   Move 1 LED forward
+   Move 1 LED backward
+   Move LED-Hole Forward
+   Move LED-Hole Backward
+   All Off
 
+   The Infra red distance sensor changes the brightness
+   of the LEDs when it senses something
+   LEDs get dimmer the closer something gets.
 
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
+   
  */
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 8;     // the number of the pushbutton pin
-const int green =  11;// the number of the LED pin
-const int yellow = 10;
-const int red = 9;
-const int sensorPin = A0;
+const int buttonPin = 8;   // the number of the pushbutton pin
+const int green =  11;     // the number of the green LED pin
+const int yellow = 10;     // the number of the yellow LED pin
+const int red = 9;         // the number of the red LED pin
+const int sensorPin = A0;  // the number of the IR sensor pin
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-int LEDState = 0;
-int previousButtonState = 0;
-int timer; 
-int sensorValue;
+int buttonState = 0;       // variable for pushbutton status
+int LEDState = 0;          // variable for LED state
+int previousButtonState = 0; // previous button state variable
+int timer;                 // timer variable
+int sensorValue;           //value fo IR sensor
+
 void setup() {
   // initialize the LED pin as an output:
   pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(red, OUTPUT);
+  
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+  
   // initialize Serial Communication
   Serial.begin(9600);
 }
 
+
 void writeAll(bool redon, bool yellowon, bool greenon){
+  //Sets LEDs to PWM signal between 0 and 255 based on IR sensor 
   analogWrite(red, redon ? (250-sensorValue/2) : 0);
   analogWrite(yellow, yellowon ? (250-sensorValue/2) : 0);
   analogWrite(green, greenon ? (250-sensorValue/2) : 0);
@@ -59,54 +68,77 @@ void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
   sensorValue = analogRead(sensorPin);
-  //Serial.println(sensorValue);
+  
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if ((buttonState == HIGH) && (previousButtonState == LOW)) {
+    
       //increment LEDState
       LEDState++;
+
+      //LED State counts 0-8, 9 total states
       LEDState = LEDState % 9;
-      Serial.println(LEDState);
+
+      //delays 2ms so button doesnt read
+      //multiple times
+      delay(2);
   }
 
+  //times on half second intervals
   timer = millis()/500;
-  if (LEDState == 0)
-  {
+
+  //These set states for LEDs
+  if (LEDState == 0) {
+    //all LEDs On
     writeAll(HIGH, HIGH, HIGH);
+    
   }
-  else if(LEDState == 1)
-  {
+  else if(LEDState == 1) {
+    //Blinking LEDs
     writeAll(timer%2 > 0, timer%2 > 0, timer%2 > 0);
+    
   }
-  else if(LEDState == 2)
-  {
+  else if(LEDState == 2) {
+    //Counts forward in Binary
     writeAll(timer%2 > 0,timer%4 > 1, timer%8 > 3);
+    
   }
-  else if(LEDState == 3)
-  {
+  else if(LEDState == 3) {
+    //Counts backward in Binary
     writeAll(!(timer%2 > 0),!(timer%4 > 1), !(timer%8 > 3)); 
+    
   }
-  else if(LEDState == 4)
-  {
+  else if(LEDState == 4) {
+    //Blinks 1 LED forward
+    //1-0-0 -> 0-1-0 -> 0-0-1
     writeAll(timer%3 == 0, timer%3 == 1, timer%3 == 2);   
+    
   }
-  else if(LEDState == 5)
-  {
+  else if(LEDState == 5) {
+    //Blinks 1 LED backward
+    //0-0-1 -> 0-1-0 -> 1-0-0
     writeAll(timer%3 == 2, timer%3 == 1, timer%3 == 0);
+    
   }
-  else if(LEDState == 6)
-  {
+  else if(LEDState == 6) {
+    //Blinks LED hole forward
+    //0-1-1 -> 1-0-1 -> 1-1-0
     writeAll(!(timer%3 == 0), !(timer%3 == 1), !(timer%3 == 2));
+    
   }
-  else if(LEDState == 7)
-  {
+  else if(LEDState == 7) {
+    //Blinks LED hole backward
+    //1-1-0 -> 1-0-1 -> 0-1-1
     writeAll(!(timer%3 == 2), !(timer%3 == 1), !(timer%3 == 0));
+    
   }
-  else
-  {
+  else {
+    //All LEDs Off
     writeAll(LOW, LOW, LOW);
+    
   }
   
+  //Sets previous button state to current
+  //button state for use in future loops
   previousButtonState = buttonState;
-  delay(2);
 }
